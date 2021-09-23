6f6ab95a1f60af87d44dd64f050e0123a0c07003
Alfred Warui <alfredwarui17@gmail.com>
2021-09-24 02:15:31 +0300

    create basic app configurations

---
+++ config.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+#Dotenv Configuration
+env_path = Path('.')/'.env'
+load_dotenv(dotenv_path=env_path)
+
+class Config: 
+  '''
+  General configuration class
+  '''
+  SECRET_KEY = os.getenv("SECRET_KEY") #secret key for wtf forms
+  UPLOADED_PHOTOS_DEST = 'app/static/photos' #storage location of uploaded photos in the app
+  
+#  app email configurations
+  MAIL_SERVER = 'smtp.googlemail.com'
+  MAIL_PORT = 587
+  MAIL_USE_TLS = True
+  MAIL_USERNAME = os.getenv("MAIL_USERNAME")
+  MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+
+class ProdConfig(Config): 
+  '''
+  Test configuration child class
+  '''
+  pass
+
+class TestConfig(Config): 
+  '''
+  Production configuration child class
+  '''
+  pass
+
+class DevConfig(Config): 
+  '''
+  Development configuration child class
+  '''
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://toshiba:@localhost/blogs'
+  DEBUG = True
+  
+config_options = {
+  'development':DevConfig,
+  'production': ProdConfig,
+  'test':TestConfig
+}
