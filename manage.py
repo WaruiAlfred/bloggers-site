1549565e82e15e2912af115dc99a119cef6e85fd
Alfred Warui <alfredwarui17@gmail.com>
2021-09-24 18:25:49 +0300

    create app run file

---
+++ manage.py
@@ -0,0 +1,28 @@
+from app import create_app,db
+from flask_script import Manager,Server
+from app.models import User,Blogs,Comments
+from flask_migrate import Migrate,MigrateCommand
+
+# App instance created
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+@manager.command
+def test(): 
+  '''Run the unit tests.'''
+  import unittest
+  tests = unittest.TestLoader().discover('tests')
+  unittest.TextTestRunner(verbosity=2).run(tests)
+
+#flask-script shell
+@manager.shell
+def make_shell_context(): 
+  return dict(app = app, db = db, User = User, Blogs = Blogs, Comments = Comments)
+
+# Setting up Flask-Migrate to create a new database schema
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+if __name__ == '__main__': 
+  manager.run()
